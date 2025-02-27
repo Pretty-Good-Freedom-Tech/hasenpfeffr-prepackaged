c0983975bddc662a4e0c37edf47a74a6ccf12308
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-27 13:43:53 -0500

    Create calculateHops.sh

---
+++ algos/calculateHops.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set Neo4j connection details
+NEO4J_URI="bolt://localhost:7687"
+NEO4J_USER="neo4j"
+NEO4J_PASSWORD="neo4jneo4j"
+
+source /etc/hasenpfeffr.conf
+
+CYPHER1="MATCH (u:NostrUser) SET u.hops=999"
+CYPHER2="MATCH (u:NostrUser {pubkey:'$GRAPEVINE_REFERENCE_PUBKEY'}) SET u.hops=0"
+CYPHER3="MATCH (u1:NostrUser)-[:FOLLOWS]->(u2:NostrUser) WHERE u2.hops - u1.hops > 1 SET u2.hops = u1.hops + 1 RETURN count(u2) as numUpdates"
+
+numHops=1
+
+sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER1"
+sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER2"
+cypherResults=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER3")
+numUpdates="${cypherResults:11}"
+
+echo "hop # $numHops; numUpdates: $numUpdates"
+
+while [[ "$numUpdates" -gt 0 ]] && [[ "$numHops" -lt 12 ]];
+do
+    ((numHops++))
+    cypherResults=$(sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER3")
+    numUpdates="${cypherResults:11}"
+    echo "hop # $numHops; numUpdates: $numUpdates"
+done
