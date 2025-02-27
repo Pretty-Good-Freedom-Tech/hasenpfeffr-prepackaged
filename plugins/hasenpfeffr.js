45872eb3641df7dcf64d9f9bd541454d2fe46b91
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-27 01:49:14 -0500

    Create hasenpfeffr.js

---
+++ plugins/hasenpfeffr.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+const fs = require('node:fs');
+
+const whitelist_pubkeys = JSON.parse(fs.readFileSync('./whitelist_pubkeys.json', 'utf8'))
+
+const blacklist_pubkeys = JSON.parse(fs.readFileSync('./blacklist_pubkeys.json', 'utf8'))
+
+const whitelist_kinds = [3, 1984, 10000, 10040, 30382, 30383, 30384]
+
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', (line) => {
+    let req = JSON.parse(line);
+
+    if (req.type !== 'new') {
+        console.error("unexpected request type"); // will appear in strfry logs
+        return;
+    }
+
+    let res = { id: req.event.id };
+
+    res.action = 'reject'
+
+    if (whitelist_kinds.includes(req.event.kind)) {
+        res.action = 'accept';
+    }
+
+    if (whitelist_pubkeys[req.event.pubkey]) {
+        res.action = 'accept';
+    }
+
+    if (blacklist_pubkeys[req.event.pubkey]) {
+        res.action = 'reject';
+    }
+
+    if (res.action == 'reject') {
+        res.msg = 'blocked by the hasenpfeffr plugin';
+    }
+
+    console.log(JSON.stringify(res));
+});
