00ca3e341d9bfec186f0f0b2fed1c358167b1322
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-27 02:03:46 -0500

    Create loadJsonIntoNeo4j.sh

---
+++ pipeline/batch/loadJsonIntoNeo4j.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set Neo4j connection details
+NEO4J_URI="bolt://localhost:7687"
+NEO4J_USER="neo4j"
+NEO4J_PASSWORD="neo4jneo4j"
+
+sudo cypher-shell -u neo4j -p neo4jneo4j -a bolt://localhost:7687 -f /home/ubuntu/strfry/pipeline/initialization/apocCypherCommand > /dev/null
