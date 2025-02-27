b66baff8ff720aa24dda11067534c62697c45f37
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-27 02:07:42 -0500

    Update updateNostrUserKind3Data.sh

--- pipeline/batch/updateNostrUserKind3Data.sh
+++ pipeline/batch/updateNostrUserKind3Data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Set Neo4j connection details
-NEO4J_URI="bolt://localhost:7687"
-NEO4J_USER="neo4j"
-NEO4J_PASSWORD="neo4jneo4j"
+source /etc/hasenpfeffr.conf # NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 
 sudo cypher-shell -u neo4j -p neo4jneo4j -a bolt://localhost:7687 -f apocCypherCommand2 > /dev/null
