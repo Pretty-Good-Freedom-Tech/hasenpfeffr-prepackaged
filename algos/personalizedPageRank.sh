e063836521520b6d00988677b6ee0962bdd5a799
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-27 02:48:03 -0500

    Update personalizedPageRank.sh

--- algos/personalizedPageRank.sh
+++ algos/personalizedPageRank.sh
@@ -1,28 +1,21 @@
 #!/bin/bash
 
-# Set Neo4j connection details
-NEO4J_URI="bolt://localhost:7687"
-NEO4J_USER="neo4j"
-NEO4J_PASSWORD="neo4jneo4j"
-
-pubkey="e5272de914bd301755c439b88e6959a43c9d2664831f093c51e9c799a16a102f"
-
-source /etc/hasenpfeffr.conf
+source /etc/hasenpfeffr.conf # NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD, GRAPEVINE_REFERENCE_PUBKEY
 
 echo "GRAPEVINE_REFERENCE_PUBKEY: $GRAPEVINE_REFERENCE_PUBKEY"
 
 CYPHER1="
 MATCH (source:NostrUser)-[r:FOLLOWS]->(target:NostrUser)
 RETURN gds.graph.project(
-  'personalizedPageRank_$pubkey',
+  'personalizedPageRank_$GRAPEVINE_REFERENCE_PUBKEY',
   source,
   target
 )
 "
 
 CYPHER2="
-MATCH (refUser:NostrUser {pubkey: '$pubkey'})
-CALL gds.pageRank.write('personalizedPageRank_$pubkey', {
+MATCH (refUser:NostrUser {pubkey: '$GRAPEVINE_REFERENCE_PUBKEY'})
+CALL gds.pageRank.write('personalizedPageRank_$GRAPEVINE_REFERENCE_PUBKEY', {
   maxIterations: 20,
   dampingFactor: 0.85,
   scaler: 'MinMax',
@@ -33,11 +26,7 @@ YIELD nodePropertiesWritten, ranIterations
 RETURN nodePropertiesWritten, ranIterations
 "
 
-CYPHER3="CALL gds.graph.drop('personalizedPageRank_$pubkey') YIELD graphName"
-
-# echo "CYPHER1: $CYPHER1"
-# echo "CYPHER2: $CYPHER2"
-# echo "CYPHER3: $CYPHER3"
+CYPHER3="CALL gds.graph.drop('personalizedPageRank_$GRAPEVINE_REFERENCE_PUBKEY') YIELD graphName"
 
 sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER1"
 sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER2"
