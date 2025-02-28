cbf3b1325f8f64f33d48d6d2dad2a326323ebef4
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-28 10:53:49 -0500

    Update load.sh

--- pipeline/batch/load.sh
+++ pipeline/batch/load.sh
@@ -3,7 +3,7 @@
 # This takes several hours and loads all kind 3 events into strfry from scratch
 # the --recent option on the createAllKind3EventsStripped.sh only transfers the most recent events
 
-sudo ./createAllKind3EventsStripped.sh "$1" "$2"
+sudo ./strfryToKind3Events.sh "$1" "$2"
 sudo ./kind3EventsToJson.sh
 sudo ./loadJsonIntoNeo4j.sh
 sudo ./updateNostrUserKind3Data.sh
