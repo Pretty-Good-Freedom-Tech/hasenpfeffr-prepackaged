312540071f07c35e36e4a1a8083a6cfb11ea3d24
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-28 02:54:35 -0500

    Update negentropySync.sh

--- pipeline/batch/negentropySync.sh
+++ pipeline/batch/negentropySync.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 # adapted from https://github.com/hoytech/strfry/blob/master/README.md#sync
+
+# use service to set up cron job (infrequently - maybe once a day)
 
 sudo strfry sync wss://relay.primal.net --filter '{"kinds":[3,1984,10000]}'
