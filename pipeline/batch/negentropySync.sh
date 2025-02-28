5f03e72db2a64e0b565c33e6c6b0ae9d7dbeb23a
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-28 02:55:37 -0500

    Update negentropySync.sh

--- pipeline/batch/negentropySync.sh
+++ pipeline/batch/negentropySync.sh
@@ -4,4 +4,8 @@
 
 # use service to set up cron job (infrequently - maybe once a day)
 
+sudo systemctl stop addToQueue.service
+
 sudo strfry sync wss://relay.primal.net --filter '{"kinds":[3,1984,10000]}'
+
+sudo systemctl start addToQueue.service
