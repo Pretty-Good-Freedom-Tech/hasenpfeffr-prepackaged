ef2ce0404cf669ca6fa3e86f9bf6506ee26764ca
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-28 11:05:41 -0500

    Delete pipeline/batch/followsToNeo4j_deprecating.sh

--- pipeline/batch/followsToNeo4j_deprecating.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /etc/hasenpfeffr.conf # NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
-
-sudo cypher-shell -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" -a "$NEO4J_URI" -f apocCypherCommand1 > /dev/null
