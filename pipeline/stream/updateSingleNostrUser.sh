fe98902baf6e2802b1d92cb71eaa47507f5a7934
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-02-28 02:23:02 -0500

    Update updateSingleNostrUser.sh

--- pipeline/stream/updateSingleNostrUser.sh
+++ pipeline/stream/updateSingleNostrUser.sh
@@ -5,7 +5,7 @@
 
 source /etc/hasenpfeffr.conf # NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 
-path_to_queue="./queue/"
+path_to_queue="/home/ubuntu/hasenpfeffr/pipeline/stream/queue/"
 
 pk_next_full_path=$(find "$path_to_queue" -type f -print0 | xargs -0 stat -c "%Y %n" | sort -n | head -n 1 | cut -d " " -f 2-)
 
@@ -31,38 +31,31 @@ PUBKEY=$(echo $kind3Event | jq '.pubkey' | tr -d '"')
 CREATED_AT=$(echo $kind3Event | jq '.created_at' | tr -d '"')
 TAGS=$(echo $kind3Event | jq '.tags' | jq -c .)
 
-> newFollowListV2.json
+> newFollowList.json
 
 for item in $(echo $TAGS | jq -c '.[]'); do
     tag=$(echo $item | jq '.[0]' | tr -d '"')
     if [ "$tag" == "p" ]; then
         pk_followee=$(echo $item | jq '.[1]' | tr -d '"')
-        echo "{ pk_follower: \"$pk_follower\", pk_followee: \"$pk_followee\" }" >> newFollowListV2.json
+        echo "{ pk_follower: \"$pk_follower\", pk_followee: \"$pk_followee\" }" >> newFollowList.json
     fi
 done
 
-sudo mv newFollowListV2.json /var/lib/neo4j/import/newFollowListV2.json
+sudo mv newFollowList.json /var/lib/neo4j/import/newFollowList.json
 
-sudo chown ubuntu:ubuntu /var/lib/neo4j/import/newFollowListV2.json
+sudo chown ubuntu:ubuntu /var/lib/neo4j/import/newFollowList.json
 
 CYPHER1="MATCH (u1:NostrUser {pubkey:'$pk_follower'})-[r:FOLLOWS]->(u2:NostrUser) DELETE r"
 
 sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD"  "$CYPHER1"
 
-sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" -f /home/ubuntu/strfry/brainstorm/actions/nodes/nostrUsers/apocCypherCommandV2
+sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" -f /home/ubuntu/hasenpfeffr/pipeline/stream/apocCypherCommand
 
 CYPHER2="MATCH (u:NostrUser {pubkey:'$pk_follower'}) SET u.kind3EventId='$EVENT_ID', u.kind3CreatedAt=$CREATED_AT "
 
 sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD"  "$CYPHER2"
 
-
-# OPTIONAL: get followers, kind3EventId and kind3CreatedAt from neo4j
-
-# delete all followers from neo4j
-
-# add all new followers to neo4j
-
 # clean up
 
 sudo rm "$pk_next_full_path"
-sudo rm /var/lib/neo4j/import/newFollowListV2.json
+sudo rm /var/lib/neo4j/import/newFollowList.json
