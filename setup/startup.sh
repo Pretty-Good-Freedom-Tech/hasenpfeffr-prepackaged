1b69797efe6257f6d25e66b35686bba91a157786
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-03-01 14:46:59 -0500

    Update startup.sh

--- setup/startup.sh
+++ setup/startup.sh
@@ -9,5 +9,6 @@ sudo mkdir /var/log/hasenpfeffr
 sudo touch /var/log/hasenpfeffr/calculateHops.log
 sudo touch /var/log/hasenpfeffr/personalizedPageRank.log
 
+# load CALCULATE_HOPS_TIMER and PERSONALIZED_PAGERANK_TIMER into their respective timers
 sudo /home/ubuntu/hasenpfeffr/algos/update-calculateHops-timer.sh
 sudo /home/ubuntu/hasenpfeffr/algos/update-personalizedPageRank-timer.sh
