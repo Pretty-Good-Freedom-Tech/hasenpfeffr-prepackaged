13573034e15ee516fbc9d737c6d6c5c5271e4db2
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-03-01 11:18:45 -0500

    Update files.sh

--- setup/files.sh
+++ setup/files.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# commands to run at startup
+
 # various chmod, chown and mv commands to do at startup
+
+# create log files
+sudo mkdir /var/log/hasenpfeffr
+sudo touch /var/log/hasenpfeffr/calculateHops.log
+sudo touch /var/log/hasenpfeffr/personalizedPageRank.log
