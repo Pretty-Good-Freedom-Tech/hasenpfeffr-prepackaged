597ffc30b0d83f808000d2ebd324acf75de0316d
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-03-01 14:49:04 -0500

    Update startup.sh

--- setup/startup.sh
+++ setup/startup.sh
@@ -12,3 +12,6 @@ sudo touch /var/log/hasenpfeffr/personalizedPageRank.log
 # load CALCULATE_HOPS_TIMER and PERSONALIZED_PAGERANK_TIMER into their respective timers
 sudo /home/ubuntu/hasenpfeffr/algos/update-calculateHops-timer.sh
 sudo /home/ubuntu/hasenpfeffr/algos/update-personalizedPageRank-timer.sh
+
+# create update-reconciliation-timer.sh for reconciliation
+# first merge runReconciliaiton and processReconcileQueue into one timer
