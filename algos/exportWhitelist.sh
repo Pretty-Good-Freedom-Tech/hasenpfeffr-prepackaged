f88ce8fde7e1b1b20c32f51645cda87e7b105245
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-03-02 14:25:44 -0500

    Update exportWhitelist.sh

--- algos/exportWhitelist.sh
+++ algos/exportWhitelist.sh
@@ -2,7 +2,7 @@
 
 source /etc/hasenpfeffr.conf # NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD
 
-echo "Running exportWhiteList. This script updates ~/hasenpfeffr/plugins/whitelist_pubkeys.json based on personalizedPageRank scores stored in neo4j."
+echo "Running exportWhiteList. This script updates /home/ubuntu/hasenpfeffr/plugins/whitelist_pubkeys.json based on personalizedPageRank scores stored in neo4j."
 
 CYPHER1="
 MATCH (n:NostrUser) WHERE n.personalizedPageRank > 0.000001
@@ -10,17 +10,17 @@ RETURN n.personalizedPageRank, n.pubkey
 ORDER BY n.personalizedPageRank DESC
 "
 
-sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER1" | tail -n +2 > whitelistQueryOutput.txt
+sudo cypher-shell -a "$NEO4J_URI" -u "$NEO4J_USER" -p "$NEO4J_PASSWORD" "$CYPHER1" | tail -n +2 > /home/ubuntu/hasenpfeffr/algos/whitelistQueryOutput.txt
 
 # create whitelist
 
-> whitelist_pubkeys.json
+> /home/ubuntu/hasenpfeffr/algos/whitelist_pubkeys.json
 
-echo "{" >> whitelist_pubkeys.json
+echo "{" >> /home/ubuntu/hasenpfeffr/algos/whitelist_pubkeys.json
 
 # iterate through all pubkeys and add to json file
 
-numLines=$(wc -l whitelistQueryOutput.txt | awk '{print $1}')
+numLines=$(wc -l /home/ubuntu/hasenpfeffr/algos/whitelistQueryOutput.txt | awk '{print $1}')
 
 whichLine=1
 while read -r p;
@@ -30,16 +30,15 @@ do
   IFS='"'
   read -ra array2 <<< "$p"
   if [ "$whichLine" -lt "$numLines" ]; then
-    echo "  \"${array2[1]}\": true," >> whitelist_pubkeys.json
+    echo "  \"${array2[1]}\": true," >> /home/ubuntu/hasenpfeffr/algos/whitelist_pubkeys.json
   else
-    echo "  \"${array2[1]}\": true" >> whitelist_pubkeys.json
+    echo "  \"${array2[1]}\": true" >> /home/ubuntu/hasenpfeffr/algos/whitelist_pubkeys.json
   fi
   ((whichLine++))
-done < whitelistQueryOutput.txt
+done < /home/ubuntu/hasenpfeffr/algos/whitelistQueryOutput.txt
 
-echo "}" >> whitelist_pubkeys.json
+echo "}" >> /home/ubuntu/hasenpfeffr/algos/whitelist_pubkeys.json
 
-sudo mv whitelist_pubkeys.json ~/hasenpfeffr/plugins/whitelist_pubkeys.json
-
+sudo mv /hume/ubuntu/hasenpfeffr/algos/whitelist_pubkeys.json /home/ubuntu/hasenpfeffr/plugins/whitelist_pubkeys.json
 # clean up
-sudo rm whitelistQueryOutput.txt
+sudo rm /home/ubuntu/hasenpfeffr/algos/whitelistQueryOutput.txt
