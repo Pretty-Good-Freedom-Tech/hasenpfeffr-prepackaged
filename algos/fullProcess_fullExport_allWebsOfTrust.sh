6b2e65a735825c08f287069a596362c97a89e111
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2025-03-02 14:20:56 -0500

    Create fullProcess_fullExport_allWebsOfTrust.sh

---
+++ algos/fullProcess_fullExport_allWebsOfTrust.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Perform all of the following steps:
+
+# calculateHops.sh
+# calculatePersonalizedPageRank.sh
+# calculatePersonalizedGrapeRank.sh (forthcoming)
+# exportWhitelist.sh
+# exportNip85.sh (forthcoming)
+
+sudo /home/ubuntu/hasenpfeffr/algos/calculateHops.sh
+
+sudo /home/ubuntu/hasenpfeffr/algos/calculatePersonalizedPageRank.sh
+
+# forthcoming:
+# sudo /home/ubuntu/hasenpfeffr/algos/calculatePersonalizedGrapeRank.sh
+
+sudo /home/ubuntu/hasenpfeffr/algos/exportWhitelist.sh
+
+# forthcoming:
+# sudo /home/ubuntu/hasenpfeffr/algos/exportNip85.sh
